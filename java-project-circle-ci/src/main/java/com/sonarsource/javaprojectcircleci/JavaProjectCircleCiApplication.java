53a027e4e68b8ae28ffc666b6fadb6d3af957434
Tom Van Braband <tom.vanbraband@sonarsource.com>
2020-10-09 15:06:14 +0200

    init

---
+++ java-project-circle-ci/src/main/java/com/sonarsource/javaprojectcircleci/JavaProjectCircleCiApplication.java
@@ -0,0 +1,13 @@
+package com.sonarsource.javaprojectcircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectCircleCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectCircleCiApplication.class, args);
+	}
+
+}
---
+++ java-project-circle-ci/src/test/java/com/sonarsource/javaprojectcircleci/JavaProjectCircleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarsource.javaprojectcircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectCircleCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
